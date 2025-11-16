80675f5087793d2b861908d201d4dabfa27edbf6
hooangnguyen <hoangvn950@gmail.com>
2025-11-17 00:33:05 +0700

    Add project files.

---
+++ ADPD code/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ADPD_code.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
